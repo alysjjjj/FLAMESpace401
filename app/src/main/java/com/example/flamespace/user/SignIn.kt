9fa6ed2fd4c1f4f5cf9991291e90b94eadb85b0b
alyjjjj <altu.merto.up@phinmaed.com>
2025-04-01 21:55:46 +0800

    twilio

--- app/src/main/java/com/example/flamespace/user/SignIn.kt
+++ app/src/main/java/com/example/flamespace/user/SignIn.kt
@@ -23,7 +23,7 @@ class SignIn : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
-        // Initialize Firebasse reference
+        // Initialize Firebase reference
         database = FirebaseDatabase.getInstance("https://flamespace-590f0-default-rtdb.firebaseio.com/").reference.child("users")
 
         val etEmail = findViewById<EditText>(R.id.et_email)
